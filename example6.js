36f5240b266a0519241f39a3600e8691917c39e2
Roger Hardiman <software@rjh.org.uk>
2019-11-07 13:47:36 +0000

    Show camera make/model when connecting

--- example6.js
+++ example6.js
@@ -1,16 +1,20 @@
 /**
  * NodeJS ONVIF Events
+ * Reveive Events using a PullPoint Subscription and display the events on screen
+ * Tested with Axis (which uses a fixed PullPoint URL with a SubscriberId in the XML)
+ * and with HikVision (which uses a dynamically generated PullPoint URL)
  *
  * Created by Roger Hardiman <opensource@rjh.org.uk>
- *
- * Register for Events and write them to stdout
  * 
+ * (c) Roger Hardiman, RJH Technical Consultancy Ltd, November 2019
+ * Licenced under the MIT Open Source Licence
+ *
  */
 
-let HOSTNAME = '192.168.1.151',
+let HOSTNAME = '127.0.0.1',
     PORT = 10101,
-    USERNAME = 'onvifuser',
-    PASSWORD = 'xxxx';
+    USERNAME = 'user',
+    PASSWORD = 'pass';
 
 let Cam = require('./lib/onvif').Cam;
 let flow = require('nimble');
@@ -20,7 +24,8 @@ new Cam({
 	username : USERNAME,
 	password : PASSWORD,
 	port : PORT,
-	timeout : 10000
+    timeout : 10000,
+    preserveAddress : true   // Enables NAT support and re-writes for PullPointSubscription URL
 }, function CamFunc(err) {
 	if (err) {
 		console.log(err);
@@ -37,14 +42,34 @@ new Cam({
     // Use Nimbe's flow to execute ONVIF commands in sequence
     flow.series([
         function(callback) {
+            cam_obj.getDeviceInformation(function(err, info, xml) {
+                if (!err) console.log('Manufacturer  ' + info.manufacturer);
+                if (!err) console.log('Model         ' + info.model);
+                if (!err) console.log('Firmware      ' + info.firmwareVersion);
+                if (!err) console.log('Serial Number ' + info.serialNumber);
+                callback();
+            });
+        },
+        function(callback) {
+            cam_obj.getSystemDateAndTime(function(err, date, xml) {
+                if (!err) console.log('Device Time   ' + date);
+                callback();
+            });
+        },
+        function(callback) {
                 cam_obj.getCapabilities(function(err, data, xml) {
                 if (err) {
                     console.log(err);
                 }
                 if (!err && data.events && data.events.WSPullPointSupport && data.events.WSPullPointSupport == true) {
-                    console.log('Camera Events service found');
+                    console.log('Camera supports WSPullPoint');
                     hasEvents = true;
+                } else {
+                    console.log('Camera does not show WSPullPoint support, but trying anyway');
+                    // Have an Axis cameras that says False to WSPullPointSuppor but supports it anyway
+                    hasEvents = true; // Hack for Axis cameras
                 }
+
                 if (hasEvents == false) {
                     console.log('This camera/NVT does not support PullPoint Events')
                 }
