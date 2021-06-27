7c56e8026a6a54945f563b61808252c854444638
Roger Hardiman <opensource@rjh.org.uk>
2021-06-27 07:21:45 +0100

    Add example of catching Discovery errors and XML parsing error

--- example4.js
+++ example4.js
@@ -10,6 +10,12 @@ var onvif = require('./lib/onvif');
 onvif.Discovery.on('device', function(cam,rinfo,xml){
 	// function will be called as soon as NVT responds
 	console.log('Reply from ' + rinfo.address);
+	console.log(JSON.stringify(rinfo));
 	console.log(cam.hostname + ':' + cam.port + cam.path);
 })
+onvif.Discovery.on('error', function (err,xml) {
+	// The ONVIF library had problems parsing some XML
+	console.log('Discovery error ' + err);
+	// console.log(xml);
+});
 onvif.Discovery.probe();
