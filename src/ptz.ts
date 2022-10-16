eeccaadbe70553af1913a91fa10bed63563ebb52
Andrew D.Laptev <a.d.laptev@gmail.com>
2022-10-16 22:31:48 +0300

    Add `setHomePosition`, `removePreset`, `gotoHomePosition`, `getStatus` methods

--- src/ptz.ts
+++ src/ptz.ts
@@ -4,7 +4,7 @@ import {
   Duration, PTZConfiguration, Space1DDescription, Space2DDescription, Vector1D, Vector2D,
 } from './media';
 
-interface PTZPresetTourSupported {
+export interface PTZPresetTourSupported {
   /** Indicates number of preset tours that can be created. Required preset tour operations shall be available for this
    * PTZ Node if one or more preset tour is supported */
   maximumNumberOfPresetTours: number;
@@ -80,8 +80,6 @@ export interface PTZSpace {
   extension?: any;
 }
 
-type ResponsePTZNode = PTZNode & {$?: ReferenceToken};
-
 export interface DurationRange {
   min: Duration;
   max: Duration;
@@ -137,7 +135,7 @@ export interface PTZVector {
 /**
  * Simplified structure of PTZ vector to use as an input argument for position and speed in movement commands.
  * */
-interface PTZInputVector {
+export interface PTZInputVector {
   /** Pan value */
   pan?: number;
   /** Synonym for pan value */
@@ -173,6 +171,48 @@ export interface SetPresetResponse {
   presetToken: ReferenceToken;
 }
 
+export interface RemovePresetOptions {
+  /** A reference to the MediaProfile where the operation should take place. */
+  profileToken?: ReferenceToken;
+  /** A requested preset token. */
+  presetToken: ReferenceToken;
+}
+
+export interface GotoHomePositionOptions {
+  /** A reference to the MediaProfile where the operation should take place. */
+  profileToken?: ReferenceToken;
+  /** A requested speed.The speed parameter can only be specified when Speed Spaces are available for the PTZ Node. */
+  speed?: PTZVector | PTZInputVector;
+}
+
+export interface SetHomePositionOptions {
+  /** A reference to the MediaProfile where the home position should be set. */
+  profileToken?: ReferenceToken;
+}
+
+export interface GetStatusOptions {
+  profileToken?: ReferenceToken;
+}
+
+type MoveStatus = string;
+
+export interface PTZMoveStatus {
+  panTilt: MoveStatus;
+  zoom: MoveStatus;
+}
+
+export interface PTZStatus {
+  /** Specifies the absolute position of the PTZ unit together with the Space references. The default absolute spaces
+   * of the corresponding PTZ configuration MUST be referenced within the Position element. */
+  position?: PTZVector;
+  /** Indicates if the Pan/Tilt/Zoom device unit is currently moving, idle or in an unknown state. */
+  moveStatus?: PTZMoveStatus;
+  /** States a current PTZ error. */
+  error?: string;
+  /** Specifies the UTC time when this status was generated. */
+  utcTime?: Date;
+}
+
 /**
  * PTZ methods
  */
@@ -206,7 +246,7 @@ export class PTZ {
     });
     this.#nodes = {};
     data[0].getNodesResponse.forEach((ptzNode: any) => {
-      const node: ResponsePTZNode = linerase(ptzNode.PTZNode[0]);
+      const node: PTZNode = linerase(ptzNode.PTZNode[0]);
       this.#nodes[node.token] = node;
     });
     return this.#nodes;
@@ -238,7 +278,8 @@ export class PTZ {
    * a Video Source Configuration. In that case, the options may additionally contain coordinate systems referring to
    * the image coordinate system described by the Video Source Configuration. If the PTZ Node supports continuous
    * movements, it shall return a Timeout Range within which Timeouts are accepted by the PTZ Node
-   * @param configurationToken Token of an existing configuration that the options are intended for
+   * @param options
+   * @param options.configurationToken Token of an existing configuration that the options are intended for
    */
   async getConfigurationOptions({ configurationToken }: { configurationToken: ReferenceToken }):
     Promise<PTZConfigurationOptions> {
@@ -251,6 +292,11 @@ export class PTZ {
     return linerase(data);
   }
 
+  /**
+   * Operation to request all PTZ presets for the PTZNode in the selected profile. The operation is supported if there
+   * is support for at least on PTZ preset by the PTZNode.
+   * @param options
+   */
   async getPresets({ profileToken }: GetPresetsOptions = {}) {
     const [data] = await this.onvif.request({
       service : 'PTZ',
@@ -293,6 +339,11 @@ export class PTZ {
     );
   }
 
+  /**
+   * Operation to go to a saved preset position for the PTZNode in the selected profile. The operation is supported if
+   * there is support for at least on PTZ preset by the PTZNode.
+   * @param options
+   */
   async gotoPreset({ profileToken, presetToken, speed }: GotoPresetOptions): Promise<void> {
     await this.onvif.request({
       service : 'PTZ',
@@ -318,11 +369,70 @@ export class PTZ {
     const [data] = await this.onvif.request({
       service : 'PTZ',
       body    : '<SetPreset xmlns="http://www.onvif.org/ver20/ptz/wsdl">'
-        + `<ProfileToken>${profileToken || this.onvif.activeSource!.profileToken}</ProfileToken>`
+        + `<ProfileToken>${profileToken ?? this.onvif.activeSource!.profileToken}</ProfileToken>`
         + `<PresetName>${presetName}</PresetName>${
           presetToken ? `<PresetToken>${presetToken}</PresetToken>` : ''
         }</SetPreset>`,
     });
     return linerase(data[0].setPresetResponse);
+  }
+
+  /**
+   * Operation to remove a PTZ preset for the Node in the selected profile. The operation is supported if the
+   * PresetPosition capability exists for teh Node in the selected profile.
+   * @param options
+   */
+  async removePreset({ profileToken = this.onvif.activeSource?.profileToken, presetToken }: RemovePresetOptions): Promise<void> {
+    await this.onvif.request({
+      service : 'PTZ',
+      body    : '<RemovePreset xmlns="http://www.onvif.org/ver20/ptz/wsdl">'
+        + `<ProfileToken>${profileToken}</ProfileToken>`
+        + `<PresetToken>${presetToken}</PresetToken>`
+        + '</RemovePreset>',
+    });
+  }
+
+  /**
+   * Operation to move the PTZ device to it's "home" position. The operation is supported if the HomeSupported element
+   * in the PTZNode is true.
+   * @param options
+   */
+  async gotoHomePosition({ profileToken = this.onvif.activeSource?.profileToken, speed }: GotoHomePositionOptions): Promise<void> {
+    await this.onvif.request({
+      service : 'PTZ',
+      body    : '<GotoHomePosition xmlns="http://www.onvif.org/ver20/ptz/wsdl">'
+        + `<ProfileToken>${profileToken}</ProfileToken>${
+          speed ? `<Speed>${PTZ.PTZVectorToXML(speed)}</Speed>` : ''
+        }</GotoHomePosition>`,
+    });
+  }
+
+  /**
+   * Operation to save current position as the home position. The SetHomePosition command returns with a failure if
+   * the “home” position is fixed and cannot be overwritten. If the SetHomePosition is successful, it is possible
+   * to recall the Home Position with the GotoHomePosition command.
+   * @param options
+   */
+  async setHomePosition({ profileToken = this.onvif.activeSource?.profileToken }: SetHomePositionOptions) {
+    await this.onvif.request({
+      service : 'PTZ',
+      body    : '<SetHomePosition xmlns="http://www.onvif.org/ver20/ptz/wsdl">'
+        + `<ProfileToken>${profileToken}</ProfileToken>`
+        + '</SetHomePosition>',
+    });
+  }
+
+  /**
+   * Operation to request PTZ status for the Node in the selected profile.
+   * @param options
+   */
+  async getStatus({ profileToken = this.onvif.activeSource?.profileToken }: GetStatusOptions = {}): Promise<PTZStatus> {
+    const [data] = await this.onvif.request({
+      service : 'PTZ',
+      body    : '<GetStatus xmlns="http://www.onvif.org/ver20/ptz/wsdl">'
+        + `<ProfileToken>${profileToken}</ProfileToken>`
+        + '</GetStatus>',
+    });
+    return linerase(data).getStatusResponse.PTZStatus;
   }
 }
