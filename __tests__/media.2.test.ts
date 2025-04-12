9255ea367f2fab1c7476d5e98a8f651cb909ce25
Andrew D.Laptev <a.d.laptev@gmail.com>
2025-04-12 15:56:50 +0300

    feat: media2.deleteProfile method

--- __tests__/media.2.test.ts
+++ __tests__/media.2.test.ts
@@ -1,3 +1,4 @@
+import { describe } from 'node:test';
 import { Onvif } from '../src';
 import { ReferenceToken } from '../src/interfaces/common';
 import { ConfigurationEnumeration, ConfigurationSet, MediaProfile } from '../src/interfaces/media.2';
@@ -68,6 +69,10 @@ describe('Profiles', () => {
       expect(Object.values(profile.configurations!).map((configuration) => configuration.token))
         .toStrictEqual(Object.values(basicProfile.configurations!).map((configuration) => configuration.token));
     });
+
+    it('should throw an error requested profile token does not exist', async () => {
+      await expect(cam.media2.addConfiguration({ profileToken : '???' })).rejects.toThrow('Profile Not Exist');
+    });
   });
 
   describe('removeConfiguration', () => {
@@ -93,9 +98,23 @@ describe('Profiles', () => {
         }],
       });
       const [profile] = (await cam.media2.getProfiles({ token : newProfileToken }));
-      console.log(profile.configurations);
       const newConfigurationLength = Object.keys(profile.configurations!).length;
       expect(newConfigurationLength).toBe(0);
+    });
+
+    it('should throw an error requested profile token does not exist', async () => {
+      await expect(cam.media2.removeConfiguration({ profileToken : '???' })).rejects.toThrow('Profile Not Exist');
+    });
+  });
+
+  describe('deleteProfile', () => {
+    it('should delete the profile', async () => {
+      const result = await cam.media2.deleteProfile({ token : newProfileToken });
+      expect(result).toBeUndefined();
+    });
+
+    it('should throw an error requested profile token does not exist', async () => {
+      await expect(cam.media2.deleteProfile({ token : '???' })).rejects.toThrow('Profile Not Exist');
     });
   });
 });
