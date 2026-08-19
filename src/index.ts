48889715740a34222ef825edc25bfb220a832945
Andrew D.Laptev <a.d.laptev@gmail.com>
2026-08-19 20:32:06 +0300

    feat: Search class

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ export * from './analytics';
 export * from './deviceio';
 export * from './display';
 export * from './actionengine';
+export * from './search';
 export * from './utils';
 export { xsany } from './utils/toOnvifXMLSchemaObject';
 // export * from './compatibility/cam'; // development frozen
